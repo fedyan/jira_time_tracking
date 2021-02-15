e54d74251f17fd1a7d77368f8ca30c9110a59006
Fedor Verbovenko <f.verbovenko@agimagroup.ru>
2021-02-15 20:16:19 +0300

    vpn notify

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
 require $_SERVER['DOCUMENT_ROOT'].'/JiraTimeTracker.php';
 require $_SERVER['DOCUMENT_ROOT'].'/DayLoggedTimeDTO.php';
 
+echo "VPN connection must be enabled!";
 
 
 $jiraTimeTracker = new JiraTimeTracker();
